ee8d21252bf592c5a20b59420322da06e16084fc
JaneMa-1266 <janema1266@gmail.com>
2023-08-10 19:25:43 +0800

    上传代码

---
+++ footer/runtime.js
@@ -0,0 +1,26 @@
+var now = new Date();
+function createtime() {
+  var grt = new Date("08/08/2023 00:00:00");
+  now.setTime(now.getTime() + 250);
+  var days = (now - grt) / 1e3 / 60 / 60 / 24,
+    dnum = Math.floor(days),
+    hours = (now - grt) / 1e3 / 60 / 60 - 24 * dnum,
+    hnum = Math.floor(hours);
+  1 == String(hnum).length && (hnum = "0" + hnum);
+  var minutes = (now - grt) / 1e3 / 60 - 1440 * dnum - 60 * hnum,
+    mnum = Math.floor(minutes);
+  1 == String(mnum).length && (mnum = "0" + mnum);
+  var seconds = (now - grt) / 1e3 - 86400 * dnum - 3600 * hnum - 60 * mnum,
+    snum = Math.round(seconds);
+  1 == String(snum).length && (snum = "0" + snum);
+  let currentTimeHtml = "";
+  (currentTimeHtml =
+    hnum < 18 && hnum >= 9
+      ? `<img class='boardsign' src='/img/上班.png' title='天行健,君子以自强不息💪'><span class='textTip'> <br> 本站居然运行了 ${dnum} 天</span><span id='runtime'> ${hnum} 小时 ${mnum} 分 ${snum} 秒 </span> <i class='fas fa-heartbeat' style='color:red'></i>`
+      : `<img class='boardsign' src='/img/下班.png' title='放松些,就当是漫游地球🌈'><span class='textTip'> <br> 本站居然运行了 ${dnum} 天</span><span id='runtime'> ${hnum} 小时 ${mnum} 分 ${snum} 秒 </span> <i class='fas fa-heartbeat' style='color:red'></i>`),
+    document.getElementById("workboard") && (document.getElementById("workboard").innerHTML = currentTimeHtml);
+}
+    // src='https://npm.elemecdn.com/anzhiyu-blog@2.0.4/img/badge/安知鱼-上班摸鱼中.svg'   src='https://npm.elemecdn.com/anzhiyu-blog@2.0.4/img/badge/安知鱼-下班啦.svg'
+setInterval(() => {
+  createtime();
+}, 250);
